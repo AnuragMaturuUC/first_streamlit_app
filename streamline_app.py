5232f24fa51628a47ad405d15ffdfdc9ab32bb6e
AnuragMaturuUC <119008713+AnuragMaturuUC@users.noreply.github.com>
2023-05-10 16:50:50 -0400

    Update streamline_app.py
    Ordering the file

--- streamline_app.py
+++ streamline_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -12,7 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -26,7 +30,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-import requests
+
 
 
 streamlit.header("Fruityvice Fruit Advice!")
@@ -40,8 +44,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 
-import snowflake.connector
+
+
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])

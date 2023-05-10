dc194f122897e68a6b447b910941b8a07562568a
AnuragMaturuUC <119008713+AnuragMaturuUC@users.noreply.github.com>
2023-05-10 15:45:46 -0400

    Update streamline_app.py
    Reordered the last 2 statements

--- streamline_app.py
+++ streamline_app.py
@@ -28,6 +28,10 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+
+streamlit.text(fruityvice_response.json())
+
+
